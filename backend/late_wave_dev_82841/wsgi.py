01e580162e6ffeb9c0131b1f776b9cc2440da7bf
Crowdbotics <engineering@crowdbotics.com>
2023-05-13 00:49:39 +0000

    Initial commit.

---
+++ backend/late_wave_dev_82841/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for late_wave_dev_82841 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'late_wave_dev_82841.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-wave-dev-82841",
+  url: "https://late-wave-dev-82841.botics.co",
+  api: "https://late-wave-dev-82841.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
